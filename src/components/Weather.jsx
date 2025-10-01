37042a579d80ca53820101043860353a1a7d60be
M Bilal <bilalsikander87@gmail.com>
2025-10-01 23:11:07 +0500

    Search-bar com

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import "./Weather.css"
+import Search_icon from "../assets/search.png"
 
 function Weather() {
   return (
-    <div>
-        <h1>Weather App</h1>
+    <div className='d-flex flex-column align-items-center p-5 rounded-3 bg-primary weather'>
+        <div class="d-flex align-items-center gap-2 search_bar">
+          <input className='border-0 fs-5 rounded-5' type="text" name="" placeholder='Search'/>
+          <img src={Search_icon} alt=""/>
+        </div>
     </div>
   )
 }
