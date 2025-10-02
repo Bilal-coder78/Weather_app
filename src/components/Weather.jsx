0c36fab96a0bd1f6bd53d1d8eb13ee9a261bad6a
M Bilal <bilalsikander87@gmail.com>
2025-10-02 19:15:09 +0500

    fetch data using api key

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./Weather.css"
 import Search_icon from "../assets/search.png"
 import Clear_icon from "../assets/clear.png"
@@ -6,24 +6,41 @@ import Humidity_icon from "../assets/humidity.png"
 import Wind_icon from "../assets/wind.png"
 
 function Weather() {
+
+  const Search = async(city)=>{
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`
+
+      const response = await fetch(url);
+      const data = await response.json()
+      console.log(data)
+    } catch (error) {
+      
+    }
+  }
+
+  useEffect(()=>{
+    Search("London");
+  },[])
+
   return (
     <div className='d-flex flex-column align-items-center p-4 rounded-3 weather'>
-        <div class="d-flex align-items-center gap-2 search_bar">
+        <div className="d-flex align-items-center gap-2 search_bar">
           <input className='border-0 fs-5 rounded-5' type="text" name="" placeholder='Search'/>
           <img src={Search_icon} alt=""/>
         </div>
         <img src={Clear_icon} alt="" className='my-3 weather-icon'/>
         <p className='temperature m-0'>16ºc</p>
         <p className='fs-1 location m-0'>London</p>
-        <div class="weather-data w-100 mt-4 d-flex align-items-center justify-content-between text-white">
-          <div class="coli d-flex align-items-start justify-content-center">
+        <div className="weather-data w-100 mt-4 d-flex align-items-center justify-content-between text-white">
+          <div className="coli d-flex align-items-start justify-content-center">
             <img src={Humidity_icon} alt="" className='mt-1'/>
             <div className='d-flex align-items-center flex-column mx-2'>
             <p className='m-0'>91 %</p>
             <p>Humidity</p>
             </div>
           </div>
-          <div class="coli d-flex align-items-start justify-content-center">
+          <div className="coli d-flex align-items-start justify-content-center">
             <img src={Wind_icon} alt=""/>
             <div className='d-flex align-items-center flex-column mx-2'>
             <p className='m-0'>3.5 km/h</p>
