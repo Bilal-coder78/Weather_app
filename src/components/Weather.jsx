306ff08e08e866a9c4ead0a011f05ab6422ab268
M Bilal <bilalsikander87@gmail.com>
2025-10-03 17:14:45 +0500

    Completed

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -12,27 +12,27 @@ import Wind_icon from "../assets/wind.png"
 function Weather() {
 
   let inputRef = useRef();
-  const [weatherdata,setWeatherdata] = useState(false)
+  const [weatherdata, setWeatherdata] = useState(false)
 
   const allIcons = {
-    "01d" : Clear_icon,
-    "01n" : Clear_icon,
-    "02d" : Cloud_icon,
-    "02n" : Cloud_icon,
-    "03d" : Cloud_icon,
-    "03n" : Cloud_icon,
-    "04d" : Drizzle_icon,
-    "04n" : Drizzle_icon,
-    "09d" : Rain_icon,
-    "09n" : Rain_icon,
-    "10d" : Rain_icon,
-    "10n" : Rain_icon,
-    "13d" : Snow_icon,
-    "13n" : Snow_icon,
+    "01d": Clear_icon,
+    "01n": Clear_icon,
+    "02d": Cloud_icon,
+    "02n": Cloud_icon,
+    "03d": Cloud_icon,
+    "03n": Cloud_icon,
+    "04d": Drizzle_icon,
+    "04n": Drizzle_icon,
+    "09d": Rain_icon,
+    "09n": Rain_icon,
+    "10d": Rain_icon,
+    "10n": Rain_icon,
+    "13d": Snow_icon,
+    "13n": Snow_icon,
   }
 
-  const Search = async(city)=>{
-    if(city === ""){
+  const Search = async (city) => {
+    if (city === "") {
       alert("Enter city name");
       return;
     }
@@ -41,46 +41,51 @@ function Weather() {
 
       const response = await fetch(url);
       const data = await response.json()
+      if (!response.ok) {
+        alert(data.message);
+        return;
+      }
       console.log(data)
       const icon = allIcons[data.weather[0].icon] || Clear_icon;
       setWeatherdata({
-        humidity : data.main.humidity,
-        windSpeed : data.wind.speed,
-        temperature : Math.floor(data.main.temp),
-        location : data.name,
-        icon : icon,
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+        temperature: Math.floor(data.main.temp),
+        location: data.name,
+        icon: icon,
       })
     } catch (error) {
-      
+      setWeatherdata(false)
+      console.error("Error in fetching weather data")
     }
   }
 
-  useEffect(()=>{
-    Search("Karachi");
-  },[])
+  useEffect(() => {
+    Search("");
+  }, [])
 
   return (
     <div className='d-flex flex-column align-items-center p-5 rounded-3 weather'>
-        <div className="d-flex align-items-center gap-2 search_bar">
-          <input className='border-0 fs-5 rounded-5' ref={inputRef} type="text" name="" placeholder='Search'/>
-          <img src={Search_icon} alt="" onClick={()=>Search(inputRef.current.value)}/>
-        </div>
-        <img src={weatherdata.icon} alt="" className='my-3 weather-icon'/>
+      <div className="d-flex align-items-center gap-2 search_bar">
+        <input className='border-0 fs-5 rounded-5' ref={inputRef} type="text" name="" placeholder='Search' />
+        <img src={Search_icon} alt="" onClick={() => Search(inputRef.current.value)} />
+      </div>
+        <img src={weatherdata.icon} alt="" className='my-3 weather-icon' />
         <p className='temperature m-0'>{weatherdata.temperature}ºc</p>
         <p className='fs-1 location m-0'>{weatherdata.location}</p>
         <div className="weather-data w-100 mt-4 d-flex align-items-center justify-content-between text-white">
           <div className="coli d-flex align-items-start justify-content-center">
-            <img src={Humidity_icon} alt="" className='mt-1'/>
+            <img src={Humidity_icon} alt="" className='mt-1' />
             <div className='d-flex align-items-center flex-column mx-2'>
-            <p className='m-0'>{weatherdata.humidity} %</p>
-            <p>Humidity</p>
+              <p className='m-0'>{weatherdata.humidity} %</p>
+              <p>Humidity</p>
             </div>
           </div>
           <div className="coli d-flex align-items-start justify-content-center">
-            <img src={Wind_icon} alt=""/>
+            <img src={Wind_icon} alt="" />
             <div className='d-flex align-items-center flex-column mx-2'>
-            <p className='m-0'>{weatherdata.windSpeed} km/h</p>
-            <p>Wind Speed</p>
+              <p className='m-0'>{weatherdata.windSpeed} km/h</p>
+              <p>Wind Speed</p>
             </div>
           </div>
         </div>
